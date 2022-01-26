54c8054488a70bae4671176fbd8138ee6245faf1
EunoChoi <64246481+EunoChoi@users.noreply.github.com>
2022-01-27 03:47:35 +0900

    fix css, fix router
    fix css
    - information screen
    - header

--- src/routes/Information.js
+++ src/routes/Information.js
@@ -8,6 +8,8 @@ import Header from '../components/Header';
 import '../css/Information.css';
 
 function Information() {
+    
+
 
     const [infoLoading, setInfoLoading] = useState(true);
 
@@ -15,6 +17,7 @@ function Information() {
     const [movieInfo, setMovieInfo] = useState([]);
     const { id } = useParams();
     //console.log(x);
+
 
     useEffect(() => {
         fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
@@ -24,8 +27,39 @@ function Information() {
                 setInfoLoading(false);
             });
     }, []);
-    console.log(movieInfo);
+
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+  
+    const [ScrollY, setScrollY] = useState(0); // window 의 pageYOffset값을 저장
+    const [ScrollActive, setScrollActive] = useState(false);
     
+    function handleScroll() {
+      if (ScrollY > 40) {
+        setScrollY(window.pageYOffset);
+        setScrollActive(true);
+      } else {
+        setScrollY(window.pageYOffset);
+        setScrollActive(false);
+      }
+    }
+    /*
+    useEffect(() => {
+      function scrollListener() {
+        window.addEventListener("scroll", handleScroll);
+      } //  window 에서 스크롤을 감시 시작
+      scrollListener(); // window 에서 스크롤을 감시
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      }; //  window 에서 스크롤을 감시를 종료
+    });
+    */
+
+
+
+
+
+    console.log(movieInfo);
     const cover = movieInfo.large_cover_image;
     const genres = movieInfo.genres;
     const year = movieInfo.year;
@@ -36,11 +70,14 @@ function Information() {
         <div className='Information'>
             {<Header />}
             <img className='bg' src={movieInfo.background_image}/>
+            
 
             {infoLoading ? 
             <span className='infoLodingPic'>⏳</span> : 
             <div>
-                <img className='info__cover' src={cover}/>
+                <img 
+                
+                className={!ScrollActive ? "info__cover" : "info__cover hidden"} src={cover}/>
                 <div className='infoBox'>
                         <div>
                             <span className='infoBox__title'>{movieInfo.title}</span>
